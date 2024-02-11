e7f4741a100d09a7f06a6beab3fd04341e18ff85
devagya <rattandevagya@gmail.com>
2024-02-11 12:36:38 +0530

    client side created

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
 const port = 8080;
 const colors = require("colors");
 app.use(cors());
-
-app.listen(port,()=>{
-    console.log(` Server is working on port ${port} `.bgWhite.black)
-})
+app.get("/", (req, res) => {
+  res.send("server is Running");
+});
+app.listen(port, () => {
+  console.log(` Server is working on port ${port} `.bgWhite.black);
+});
