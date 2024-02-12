7e756e1bae442bb2c4aa3a1556ec8eb62c9dfd2a
devagya <rattandevagya@gmail.com>
2024-02-12 12:24:38 +0530

    indexcompleted

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 const cors = require("cors");
 const port = 8080;
 const colors = require("colors");
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callended");
+  });
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("server is Running");
