9512130ec110b701e2e14ad69bad69e2236e484a
devagya <rattandevagya@gmail.com>
2024-02-19 12:31:28 +0530

    some work done

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+import "./App.css";
+import React, { useEffect, useRef, useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import Peer from "simple-peer";
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:5000");
+function App() {
+  const [me, setMe] = useState("");
+  const [stream, setstream] = useState();
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [caller, setCaller] = useState("");
+  const [callerSignal, setCallerSignal] = useState();
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [idToCall, setIdToCall] = useState("");
+  const [callEnded, setCallEnded] = useState(false);
+  const [name, setName] = useState("");
+  const myVideo = useRef();
+  const userVideo = useRef();
+  const connectionRef = useRef();
+  return (
+    <>
+      <div>calling app</div>
+    </>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-const express = require("express");
-const app = express();
-const server = require("http").createServer(app);
+const express = require("express")
+const http = require("http")
+const app = express()
+const server = http.createServer(app)
+const colors = require("colors")
 const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-const cors = require("cors");
-const port = 8080;
-const colors = require("colors");
+	cors: {
+		origin: "http://localhost:3000",
+		methods: [ "GET", "POST" ]
+	}
+})
+app.get("/",(req,res)=>{
+	res.send("Server is running")
+})
 io.on("connection", (socket) => {
-  socket.emit("me", socket.id);
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("callended");
-  });
-  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-  });
+	socket.emit("me", socket.id)
 
-  socket.on("answerCall", (data) => {
-    io.to(data.to).emit("callAccepted", data.signal);
-  });
-});
-app.use(cors());
-app.get("/", (req, res) => {
-  res.send("server is Running");
-});
-app.listen(port, () => {
-  console.log(` Server is working on port ${port} `.bgWhite.black);
-});
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
+
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+})
+
+server.listen(5000, () => console.log(" server is running on port 5000 ".bgWhite.black))
